66e803593283d75c0277d1261f9405e9b77dd7bc
Gabriel Gatbonton <115710319+gabrielgatbonton@users.noreply.github.com>
2023-07-04 17:12:53 +0800

    Added Register View: Need Work

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import CitizensView from '../views/CitizensView.vue'
 import DashboardView from '../views/DashboardView.vue'
 import ManagementView from '../views/ManagementView.vue'
+import RegisterView from '../views/RegisterView.vue'
 
 Vue.use(VueRouter)
 
@@ -25,6 +26,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     // component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/citizens/register',
+    name: 'register',
+    component: RegisterView,
   }
 ]
 
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,137 @@
+<template>
+  <div>
+    <v-container fluid class="table-title ma-2">
+      <v-row>
+        <v-col cols="auto">
+          <!-- <v-icon left>mdi-account-box-multiple</v-icon> -->
+          <span class="title">New Registrant</span>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-divider class="mx-3"></v-divider>
+    <v-form>
+      <v-container class="mx-auto mt-3 px-8">
+        <v-row>
+          <v-col cols="auto">
+            <v-icon left>mdi-folder-multiple</v-icon>
+            <span>Category</span>
+          </v-col>
+        </v-row>
+        <v-row class="mt-n3">
+          <v-col cols="12">
+            <v-select
+              label="Category"
+              :items="categories"
+              placeholder="Choose..."
+            ></v-select>
+          </v-col>
+        </v-row>
+        <v-row class="mt-n6">
+          <v-col cols="12" lg="6" md="6">
+            <v-text-field label="HUB Registrant Number"></v-text-field>
+          </v-col>
+          <v-col cols="12" lg="6" md="6">
+            <v-text-field label="Passport Number"></v-text-field>
+          </v-col>
+        </v-row>
+        <v-divider class="mx-auto my-3"></v-divider>
+        <v-row class="mt-3">
+          <v-col cols="auto">
+            <v-icon left>mdi-human-greeting-variant</v-icon>
+            <span>Personal Information</span>
+          </v-col>
+        </v-row>
+        <v-row class="mt-n3">
+          <v-col cols="3">
+            <v-text-field label="Last Name"></v-text-field>
+          </v-col>
+          <v-col cols="3">
+            <v-text-field label="First Name"></v-text-field>
+          </v-col>
+          <v-col cols="3">
+            <v-text-field label="Middle Name"></v-text-field>
+          </v-col>
+          <v-col cols="3">
+            <v-combobox
+              label="Category"
+              :items="suffixes"
+              placeholder="Choose..."
+            ></v-combobox>
+          </v-col>
+        </v-row>
+        <v-row class="mt-n3">
+          <v-col cols="3">
+            <v-text-field label="Birthday"></v-text-field>
+          </v-col>
+          <v-col cols="3">
+            <v-select label="Sex" :items="sex"></v-select>
+          </v-col>
+          <v-col cols="3">
+            <v-select label="Civil Status" :items="civil_status"></v-select>
+          </v-col>
+          <v-col cols="3">
+            <v-text-field label="Contact Number"></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row class="mt-n3">
+          <v-col cols="3">
+            <v-text-field label="TIN"></v-text-field>
+          </v-col>
+          <v-col cols="3">
+            <v-select label="Blood Type" :items="blood_type"></v-select>
+          </v-col>
+          <v-col cols="3">
+            <v-text-field label="Contact Person's Name"></v-text-field>
+          </v-col>
+          <v-col cols="3">
+            <v-text-field label="Contact Person's Number"></v-text-field>
+          </v-col>
+        </v-row>
+        <v-divider class="mx-auto my-3"></v-divider>
+        <v-row class="mt-3">
+          <v-col cols="auto">
+            <v-icon left>mdi-map-marker</v-icon>
+            <span>Address</span>
+          </v-col>
+        </v-row>
+        <v-row class="mt-n3">
+          <v-col cols="12">
+            <v-text-field label="Unit/Building/House No./Purok/Street/Subdivision"></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row class="mt-n3">
+          <v-col cols="4">
+            <v-select label="Province" :items="provinces"></v-select>
+          </v-col>
+          <v-col cols="4">
+            <v-select label="Municipality" :items="municipalites"></v-select>
+          </v-col>
+          <v-col cols="4">
+            <v-select label="Barangay" :items="baragays"></v-select>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    categories: [
+      "REST OF ADULT POPULATION (ROAP)",
+      "FRONTLINE HEALTH SERVICES (A1)",
+      "ALL SENIOR CITIZENS (A2)",
+      "PERSON WITH COMORBIDITIES (A3)",
+      "FRONTLINE PERSONNEL (A4)",
+    ],
+    suffixes: ["Sr.", "Jr.", "III", "IV", "V"],
+    sex: ["Male", "Female"],
+    civil_status: ["Single", "Married", "Divorced", "Widowed"],
+    blood_type: ["O+", "O-", "A+", "A-", "B+", "B-", "AB+", "AB-"],
+    value: null,
+  }),
+};
+</script>
+
+<style scoped></style>
