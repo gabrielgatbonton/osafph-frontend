e0b5ee356d46025a3c5f338ec2d95dd70f6610b1
Gabriel Gatbonton <115710319+gabrielgatbonton@users.noreply.github.com>
2023-07-04 12:56:43 +0800

    Added Nav Icon Functionality and Images

--- src/layouts/NavBar.vue
+++ src/layouts/NavBar.vue
@@ -1,19 +1,37 @@
 <template>
   <div>
     <v-app-bar app flat color="grey lighten-3">
-      <v-app-bar-title> OSAFPH </v-app-bar-title>
+      <v-app-bar-nav-icon @click="toggleDrawer"></v-app-bar-nav-icon>
+      <div>
+        <v-img
+          src="../assets/MCG.png"
+          width="120"
+          height="60"
+        ></v-img>
+      </div>
       <v-spacer></v-spacer>
       <v-icon large>mdi-account</v-icon>
     </v-app-bar>
-    <NavDrawer />
+    <NavDrawer :drawer="drawer" @update-drawer="updateDrawer" />
   </div>
 </template>
 
 <script>
 import NavDrawer from "../layouts/NavDrawer.vue";
 export default {
+  data: () => ({
+    drawer: false,
+  }),
   components: {
     NavDrawer,
+  },
+  methods: {
+    toggleDrawer() {
+      this.drawer = !this.drawer;
+    },
+    updateDrawer(value) {
+      this.drawer = value;
+    },
   },
 };
 </script>
--- src/layouts/NavDrawer.vue
+++ src/layouts/NavDrawer.vue
@@ -1,36 +1,63 @@
 <template>
-    <div>
-        <v-navigation-drawer v-model="drawer" app color="grey lighten-4">
-            <v-container class="grey lighten-3">
-                <v-row justify="center">
-                    <v-col align-self="center" cols="auto">
-                        <div class="title py-1">OSAFPH IMAGE</div>
-                    </v-col>
-                </v-row>
-            </v-container>
-            <v-list>
-                <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
-                    <v-list-item-icon>
-                        <v-icon>{{ link.icon }}</v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-content>
-                        <v-list-item-title>{{ link.text }}</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-list>
-        </v-navigation-drawer>
-    </div>
+  <div>
+    <v-navigation-drawer
+      v-model="localDrawer"
+      app
+      color="grey lighten-4"
+      width="200"
+    >
+      <v-container class="grey lighten-3">
+        <v-row>
+          <v-col cols="auto" class="ma-n2 pl-4">
+            <v-img
+              src="../assets/OSAFPH LOGO.png"
+              height="55"
+              width="55"
+            ></v-img>
+          </v-col>
+          <v-col cols="auto">
+            <div class="title py-1">OSAFPH</div>
+          </v-col>
+        </v-row>
+      </v-container>
+      <v-list>
+        <v-list-item
+          v-for="link in links"
+          :key="link.text"
+          router
+          :to="link.route"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ link.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ link.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
 </template>
 
 <script>
-    export default {
-        data: () => ({
-            drawer: true,
-            links: [
-            { icon: 'mdi-view-dashboard', text: 'Dashboard', route: '/' },
-            { icon: 'mdi-folder', text: 'Citizens', route: '/citizens' },
-            { icon: 'mdi-account', text: 'Management', route: '/management' },
-            ]
-        }),
-    }
+export default {
+  data: () => ({
+    links: [
+      { icon: "mdi-view-dashboard", text: "Dashboard", route: "/" },
+      { icon: "mdi-folder", text: "Citizens", route: "/citizens" },
+      { icon: "mdi-account", text: "Management", route: "/management" },
+    ],
+  }),
+  props: ["drawer"],
+  computed: {
+    localDrawer: {
+      get() {
+        return this.drawer;
+      },
+      set(value) {
+        this.$emit("update-drawer", value);
+      },
+    },
+  },
+};
 </script>
