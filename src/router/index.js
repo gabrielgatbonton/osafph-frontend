90d738826ca5edb0ff5bf835f4ca502ae30cf5f8
gabrielgatbonton <gabriel.gatbonton3262@gmail.com>
2023-07-22 12:46:31 +0800

    Debugged Route Guard

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,36 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import dashboardRoute from './modules/dashboard'
-import citizensRoute from './modules/citizens'
-import managementRoute from './modules/management'
-import LogInLayout from '../components/LandingPage.vue'
-import { checkLoggedIn } from './modules/auth-guard';
+// index.js
+import Vue from "vue";
+import VueRouter from "vue-router";
+import dashboardRoute from "./modules/dashboard";
+import citizensRoute from "./modules/citizens";
+import managementRoute from "./modules/management";
+import LogInLayout from "../components/LandingPage.vue";
+import { checkLoggedIn } from "./modules/auth-guard";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    redirect: { name: 'login' }
+    path: "/",
+    redirect: { name: "login" },
   },
   {
-    path:'/login',
-    name: 'login',
+    path: "/login",
+    name: "login",
     component: LogInLayout,
-    beforeEnter: checkLoggedIn,
   },
   ...dashboardRoute,
   ...citizensRoute,
-  ...managementRoute
-]
+  ...managementRoute,
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
 
-export default router
+// Use the checkLoggedIn global navigation guard
+router.beforeEach(checkLoggedIn);
+
+export default router;
