888aa9f43bf97aff01bfe72d829252223c216aab
Gabriel Gatbonton <115710319+gabrielgatbonton@users.noreply.github.com>
2023-07-31 09:49:50 +0800

    Revert "Merge branch 'master' of https://github.com/gabrielgatbonton/osafph-frontend"
    This reverts commit 592d88773cdc8e96f00e295260b4bc805aa332f8, reversing
    changes made to 315b630ff28c709b2fc4568cf6c8ce3839ac37ff.

--- src/layouts/NavDrawer.vue
+++ src/layouts/NavDrawer.vue
@@ -13,7 +13,6 @@
               src="../assets/OSAFPH LOGO.png"
               height="55"
               width="55"
-              contain
             ></v-img>
           </v-col>
           <v-col cols="auto">
