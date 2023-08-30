1b0a4e123fbb689821f94e62c0586fbbe6d4aff8
gabrielgatbonton <115710319+gabrielgatbonton@users.noreply.github.com>
2023-08-30 22:37:18 +0800

    Fixed not saving on Vaccine & Booster

--- src/mixins/VaccinationValidation.js
+++ src/mixins/VaccinationValidation.js
@@ -10,15 +10,13 @@ export default {
       vaccination_site_1: { required },
       healthcare_professional_1: { required },
       healthcare_professional_license_number_1: { required },
-      dose_2: { required },
-      date_2: { required },
-      vaccine_2: { required },
-      lot_number_2: { required },
-      vaccination_site_2: { required },
-      vaccine_id_1: { required },
-      vaccine_id_2: { required },
-      healthcare_professional_2: { required },
-      healthcare_professional_license_number_2: { required },
+      dose_2: {},
+      date_2: {},
+      vaccine_2: {},
+      lot_number_2: {},
+      vaccination_site_2: {},
+      healthcare_professional_2: {},
+      healthcare_professional_license_number_2: {},
     },
   },
   computed: {
@@ -32,12 +30,12 @@ export default {
           errors.dose_1.push("Dose is required");
       }
 
-      //Dose 2
-      errors.dose_2 = [];
-      if (this.$v.responses.dose_2.$dirty) {
-        !this.$v.responses.dose_2.required &&
-          errors.dose_2.push("Dose is required");
-      }
+      // //Dose 2
+      // errors.dose_2 = [];
+      // if (this.$v.responses.dose_2.$dirty) {
+      //   !this.$v.responses.dose_2.required &&
+      //     errors.dose_2.push("Dose is required");
+      // }
 
       //Date 1
       errors.date_1 = [];
@@ -46,12 +44,12 @@ export default {
           errors.date_1.push("Date is required");
       }
 
-      //Date 2
-      errors.date_2 = [];
-      if (this.$v.responses.date_2.$dirty) {
-        !this.$v.responses.date_2.required &&
-          errors.date_2.push("Date is required");
-      }
+      // //Date 2
+      // errors.date_2 = [];
+      // if (this.$v.responses.date_2.$dirty) {
+      //   !this.$v.responses.date_2.required &&
+      //     errors.date_2.push("Date is required");
+      // }
 
       //Vaccination 1
       errors.vaccine_1 = [];
@@ -60,12 +58,12 @@ export default {
           errors.vaccine_1.push("Vaccine is required");
       }
 
-      //Vaccination 2
-      errors.vaccine_2 = [];
-      if (this.$v.responses.vaccine_2.$dirty) {
-        !this.$v.responses.vaccine_2.required &&
-          errors.vaccine_2.push("Vaccine is required");
-      }
+      // //Vaccination 2
+      // errors.vaccine_2 = [];
+      // if (this.$v.responses.vaccine_2.$dirty) {
+      //   !this.$v.responses.vaccine_2.required &&
+      //     errors.vaccine_2.push("Vaccine is required");
+      // }
 
       //Lot number 1
       errors.lot_number_1 = [];
@@ -74,12 +72,12 @@ export default {
           errors.lot_number_1.push("Lot number is required");
       }
 
-      //Lot number 2
-      errors.lot_number_2 = [];
-      if (this.$v.responses.lot_number_2.$dirty) {
-        !this.$v.responses.lot_number_2.required &&
-          errors.lot_number_2.push("Lot number is required");
-      }
+      // //Lot number 2
+      // errors.lot_number_2 = [];
+      // if (this.$v.responses.lot_number_2.$dirty) {
+      //   !this.$v.responses.lot_number_2.required &&
+      //     errors.lot_number_2.push("Lot number is required");
+      // }
 
       //Vaccine Site 1
       errors.vaccination_site_1 = [];
@@ -88,12 +86,12 @@ export default {
           errors.vaccination_site_1.push("Vaccination site is required");
       }
 
-      //Vaccine Site 2
-      errors.vaccination_site_2 = [];
-      if (this.$v.responses.vaccination_site_2.$dirty) {
-        !this.$v.responses.vaccination_site_2.required &&
-          errors.vaccination_site_2.push("Vaccination site is required");
-      }
+      // //Vaccine Site 2
+      // errors.vaccination_site_2 = [];
+      // if (this.$v.responses.vaccination_site_2.$dirty) {
+      //   !this.$v.responses.vaccination_site_2.required &&
+      //     errors.vaccination_site_2.push("Vaccination site is required");
+      // }
 
       //Healthcare 1
       errors.healthcare_professional_1 = [];
@@ -104,14 +102,14 @@ export default {
           );
       }
 
-      //Healthcare 2
-      errors.healthcare_professional_2 = [];
-      if (this.$v.responses.healthcare_professional_2.$dirty) {
-        !this.$v.responses.healthcare_professional_2.required &&
-          errors.healthcare_professional_2.push(
-            "Healthcare professional is required"
-          );
-      }
+      // //Healthcare 2
+      // errors.healthcare_professional_2 = [];
+      // if (this.$v.responses.healthcare_professional_2.$dirty) {
+      //   !this.$v.responses.healthcare_professional_2.required &&
+      //     errors.healthcare_professional_2.push(
+      //       "Healthcare professional is required"
+      //     );
+      // }
 
       //Healthcare Professional License 1
       errors.healthcare_professional_license_number_1 = [];
@@ -122,14 +120,14 @@ export default {
           );
       }
 
-      //Healthcare Professional License 2
-      errors.healthcare_professional_license_number_2 = [];
-      if (this.$v.responses.healthcare_professional_license_number_2.$dirty) {
-        !this.$v.responses.healthcare_professional_license_number_2.required &&
-          errors.healthcare_professional_license_number_2.push(
-            "License number is required"
-          );
-      }
+      // //Healthcare Professional License 2
+      // errors.healthcare_professional_license_number_2 = [];
+      // if (this.$v.responses.healthcare_professional_license_number_2.$dirty) {
+      //   !this.$v.responses.healthcare_professional_license_number_2.required &&
+      //     errors.healthcare_professional_license_number_2.push(
+      //       "License number is required"
+      //     );
+      // }
 
       return errors;
     },
