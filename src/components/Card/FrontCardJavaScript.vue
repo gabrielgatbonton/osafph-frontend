58db3539b9eb2327054f9cc0b372d73c87cc812a
Gabriel Gatbonton <115710319+gabrielgatbonton@users.noreply.github.com>
2023-10-13 15:47:39 +0800

    Testing Printing of Card

--- src/components/Card/FrontCardJavaScript.vue
+++ src/components/Card/FrontCardJavaScript.vue
@@ -2,7 +2,13 @@
   <div>
     <v-dialog v-model="dialog" min-width="600" max-width="900">
       <template v-slot:activator="{ on, attrs }">
-        <v-btn dark block class="mb-2 blue darken-4" :class="{ 'disabled-button': disabledButton }" v-bind="attrs" v-on="on"
+        <v-btn
+          dark
+          block
+          class="mb-2 blue darken-4"
+          :class="{ 'disabled-button': disabledButton }"
+          v-bind="attrs"
+          v-on="on"
           ><v-icon left>mdi-smart-card-outline</v-icon>Front</v-btn
         >
       </template>
@@ -12,20 +18,26 @@
         >
         <div class="text-center mx-4 mt-4">
           <canvas
-            ref="cardCanvas"
+            ref="frontCardCanvas"
             class="canvas-border"
             style="position: relative; max-width: 100%; height: auto"
           ></canvas>
+          <canvas
+            ref="backCardCanvas"
+            class="canvas-border"
+            style="position: relative; max-width: 100%; height: auto"
+          >
+          </canvas>
+          <vue-qrcode
+            :value="QRCodeValue"
+            @change="onDataUrlChange"
+            type="image/png"
+            :color="{ dark: '#000000ff', light: '#ffffffff' }"
+            :quality="options.quality"
+            :width="options.width"
+            :margin="options.margin"
+          ></vue-qrcode>
         </div>
-
-        <!-- <div class="text-center mx-4 mt-4">
-            <canvas
-              ref="cardCanvas"
-              class="canvas-border"
-              style="position: relative; max-width: 100%; height: auto"
-            ></canvas>
-          </div> -->
-
         <!-- Button to trigger generating and printing the ID -->
         <v-card-actions>
           <v-btn dark class="blue darken-4" block @click="generateAndPrintID"
@@ -40,6 +52,7 @@
 <script>
 import format from "date-fns/format";
 import parseISO from "date-fns/parseISO";
+import VueQrcode from "vue-qrcode";
 import { mapActions, mapGetters } from "vuex";
 export default {
   props: ["registrant"],
@@ -51,39 +64,109 @@ export default {
     barangay: null,
     municipality: null,
     province: null,
+    date_1: null,
+    date_2: null,
+    vaccination_site_1: null,
+    vaccination_site_2: null,
+    vaccine_1: null,
+    vaccine_2: null,
+    lot_number_1: null,
+    lot_number_2: null,
+    tin_number: null,
+    blood_type: null,
+    emergency_name: null,
+    emergency_number: null,
+    hub_registrant_id: null,
+    qrDataURL: null,
+    options: {
+      margin: 0,
+      quality: 1,
+      width: 930,
+    },
     disabledButton: true,
     isGetImageLoaded: false,
     isGetSignatureLoaded: false,
   }),
+  components: {
+    VueQrcode,
+  },
   methods: {
     ...mapActions("card", ["fetchImage", "fetchSignature"]),
     values() {
-      this.category = `${this.registrant.citizen.category.description}`;
-      this.fullName = `${this.registrant.citizen.last_name.toUpperCase()}, ${this.registrant.citizen.first_name.toUpperCase()} ${this.registrant.citizen.middle_name.toUpperCase()}`;
-      this.birthday = `${format(
-        parseISO(this.registrant.citizen.birthday),
-        "MMMM d, yyyy"
-      ).toUpperCase()}`;
-      this.barangay = `${this.registrant.citizen.barangay.barangay_name.toUpperCase()}`;
-      this.province = `${this.registrant.citizen.barangay.municipality.province.province_name.toUpperCase()}`;
-      this.municipality = `${this.registrant.citizen.barangay.municipality.municipality_name.toUpperCase()}, ${this.province}`;
+      if (this.registrant) {
+        console.log(this.registrant);
+        if (
+          this.registrant.citizen.vaccination_stat[0] &&
+          this.registrant.citizen.vaccination_stat[1]
+        ) {
+          this.date_1 = `${format(
+            parseISO(
+              this.registrant.citizen.vaccination_stat[0].vaccination_date
+            ),
+            "MMMM d, yyyy"
+          ).toUpperCase()}`;
+          this.date_2 = `${format(
+            parseISO(
+              this.registrant.citizen.vaccination_stat[1].vaccination_date
+            ),
+            "MMMM d, yyyy"
+          ).toUpperCase()}`;
+          this.vaccination_site_1 = `${this.registrant.citizen.vaccination_stat[0].site_name.toUpperCase()}`;
+          this.vaccination_site_2 = `${this.registrant.citizen.vaccination_stat[1].site_name.toUpperCase()}`;
+          this.vaccine_1 = `${this.registrant.citizen.vaccination_stat[0].vaccine_name.toUpperCase()}`;
+          this.vaccine_2 = `${this.registrant.citizen.vaccination_stat[1].vaccine_name.toUpperCase()}`;
+          this.lot_number_1 = `${this.registrant.citizen.vaccination_stat[0].lot_no.toUpperCase()}`;
+          this.lot_number_2 = `${this.registrant.citizen.vaccination_stat[1].lot_no.toUpperCase()}`;
+        }
+        this.category = `${this.registrant.citizen.category.description}`;
+        this.fullName = `${this.registrant.citizen.last_name.toUpperCase()}, ${this.registrant.citizen.first_name.toUpperCase()} ${
+          this.registrant.citizen.middle_name
+            ? this.registrant.citizen.middle_name.toUpperCase()
+            : ""
+        } ${
+          this.registrant.citizen.suffix
+            ? this.registrant.citizen.suffix.toUpperCase()
+            : ""
+        }`;
+        this.birthday = `${format(
+          parseISO(this.registrant.citizen.birthday),
+          "MMMM d, yyyy"
+        ).toUpperCase()}`;
+        this.barangay = `${this.registrant.citizen.barangay.barangay_name.toUpperCase()}`;
+        this.province = `${this.registrant.citizen.barangay.municipality.province.province_name.toUpperCase()}`;
+        this.municipality = `${this.registrant.citizen.barangay.municipality.municipality_name.toUpperCase()}, ${
+          this.province
+        }`;
+        this.tin_number = `${
+          this.registrant.citizen.tin_number
+            ? this.registrant.citizen.tin_number
+            : "-"
+        }`;
+        this.blood_type = `${this.registrant.citizen.blood_type}`;
+        this.emergency_name = `${this.registrant.citizen.emergency_name.toUpperCase()}`;
+        this.emergency_number = `${this.registrant.citizen.emergency_number}`;
+        this.hub_registrant_id = `${this.registrant.citizen.hub_registrant_id}`;
+      }
+    },
+    onDataUrlChange(dataUrl) {
+      this.qrDataURL = dataUrl;
     },
     requestImages() {
       this.fetchImage(this.registrant.citizen.id);
       this.fetchSignature(this.registrant.citizen.id);
     },
-    drawIDOnCanvas() {
-      const canvas = this.$refs.cardCanvas;
+    drawOnCanvasFront() {
+      const canvas = this.$refs.frontCardCanvas;
       const context = canvas.getContext("2d");
 
       // Load the card template image
-      const img = new Image();
-      img.src = require("@/assets/front.jpg");
-      img.onload = () => {
+      const frontImg = new Image();
+      frontImg.src = require("@/assets/front.jpg");
+      frontImg.onload = () => {
         // Draw the card template image on the canvas
-        canvas.width = img.width;
-        canvas.height = img.height;
-        context.drawImage(img, 0, 0);
+        canvas.width = frontImg.width;
+        canvas.height = frontImg.height;
+        context.drawImage(frontImg, 0, 0);
 
         // Draw dynamic data on the canvas
         context.font = "bold 65px Arial";
@@ -111,24 +194,129 @@ export default {
         };
       };
     },
+    drawOnCanvasBack() {
+      const canvas = this.$refs.backCardCanvas;
+      const context = canvas.getContext("2d");
+      // Load the card template image
+      const backImg = new Image();
+      if (
+        this.registrant.citizen.vaccination_stat[0] &&
+        this.registrant.citizen.vaccination_stat[1]
+      ) {
+        backImg.src = require("@/assets/back.jpg");
+      } else {
+        backImg.src = require("@/assets/unvaccinated.jpg");
+      }
+      backImg.onload = () => {
+        // Draw the card template image on the canvas
+        canvas.width = backImg.width;
+        canvas.height = backImg.height;
+        context.drawImage(backImg, 0, 0);
+
+        // Draw dynamic data on the canvas
+        context.font = "bold 50px Arial";
+        context.fillStyle = "black";
+
+        // Load and draw registrant's portrait image
+        const qrImg = new Image();
+        qrImg.src = this.qrDataURL;
+        qrImg.onload = () => {
+          // Draw portrait image after it's loaded
+          context.drawImage(qrImg, 105, 205);
+
+          // Draw Hub code
+          context.fillText(this.hub_registrant_id, 350, 1200, 1700);
+
+          // Draw the rest of the data
+          if (
+            this.registrant.citizen.vaccination_stat[0] &&
+            this.registrant.citizen.vaccination_stat[1]
+          ) {
+            context.font = "bold 65px Arial";
+            context.fillText(this.date_1, 1105, 285, 1700);
+            context.fillText(this.date_2, 1105, 560, 1700);
+            context.fillText(this.vaccination_site_1, 1105, 425, 1700);
+            context.fillText(this.vaccination_site_2, 1105, 705, 1700);
+            context.fillText(this.vaccine_1, 2000, 270, 1700);
+            context.fillText(this.vaccine_2, 2000, 545, 1700);
+
+            //Allow for the Lot numbers to be set to 40px
+            context.font = "bold 50px Arial";
+            context.fillText(this.lot_number_1, 2000, 320, 1700);
+            context.fillText(this.lot_number_2, 2000, 595, 1700);
+          }
+
+          //Reset it back to 50px
+          context.font = "bold 65px Arial";
+          context.fillText(this.tin_number, 1105, 840, 1700);
+          context.fillText(this.blood_type, 1105, 980, 1700);
+          context.fillText(this.emergency_name, 1105, 1130, 1700);
+          context.fillText(this.emergency_number, 1105, 1190, 1700);
+        };
+      };
+    },
     async generateAndPrintID() {
       try {
         // Wait for a short time to ensure the canvas content is drawn
-        const canvas = this.$refs.cardCanvas;
-        const imageDataURL = canvas.toDataURL("image/jpeg", 1.0);
+        const canvasFront = this.$refs.frontCardCanvas;
+        const frontImageDataURL = canvasFront.toDataURL("image/jpeg", 1.0);
 
-        // Open a new window with the canvas content
+        const canvasBack = this.$refs.backCardCanvas;
+        const backImageDataURL = canvasBack.toDataURL("image/jpeg", 1.0);
+
+        // Open a new window
         const printWindow = window.open("", "_blank");
-        printWindow.document.open();
-        printWindow.document.write(`
-            <img src="${imageDataURL}" style="max-width: 100%; height: auto;" />
-          `);
-        printWindow.document.close();
 
-        // Add a slight delay before triggering print
+        // Create a new document within the print window
+        const printDocument = printWindow.document;
+
+        // Set up the document's head with custom styles
+        const head = printDocument.head;
+        const style = printDocument.createElement("style");
+        style.innerHTML = `
+      @page {
+        size: landscape;
+        margin: 0;
+      }
+      body {
+        margin: 0;
+        padding: 0;
+        display: flex;
+        flex-direction: column; /* Display front and back cards in a column */
+        height: 100vh;
+      }
+      img {
+        max-width: 100%;
+      }
+      .front-card {
+        display: block;
+      }
+      .back-card {
+        display: block;
+        page-break-after: always; /* Page break after the back card */
+      }
+    `;
+        head.appendChild(style);
+
+        // Add the front image to the document
+        const frontImgElement = printDocument.createElement("img");
+        frontImgElement.className = "front-card";
+        frontImgElement.src = frontImageDataURL;
+        printDocument.body.appendChild(frontImgElement);
+
+        // Add the back image to the document
+        const backImgElement = printDocument.createElement("img");
+        backImgElement.className = "back-card";
+        backImgElement.src = backImageDataURL;
+        printDocument.body.appendChild(backImgElement);
+
+        // Add a slight delay before triggering print (optional)
         await new Promise((resolve) => setTimeout(resolve, 1000));
 
+        // Print the document directly
         printWindow.print();
+
+        // Close the print window (optional)
         printWindow.close();
       } catch (error) {
         console.error(
@@ -140,26 +328,29 @@ export default {
   },
   computed: {
     ...mapGetters("card", ["getImage", "getSignature"]),
+    QRCodeValue() {
+      return `${this.$network}vaccination/${this.registrant.citizen.hub_registrant_id}`;
+    },
   },
   watch: {
     getImage(value) {
-      if(value) {
+      if (value) {
         this.isGetImageLoaded = true;
       }
 
-      if(this.isGetImageLoaded === this.isGetSignatureLoaded) {
+      if (this.isGetImageLoaded === this.isGetSignatureLoaded) {
         this.disabledButton = false;
       }
     },
     getSignature(value) {
-      if(value) {
+      if (value) {
         this.isGetSignatureLoaded = true;
       }
 
-      if(this.isGetImageLoaded === this.isGetSignatureLoaded) {
+      if (this.isGetImageLoaded === this.isGetSignatureLoaded) {
         this.disabledButton = false;
       }
-    }
+    },
   },
   created() {
     this.requestImages();
@@ -168,7 +359,8 @@ export default {
     this.values();
   },
   updated() {
-    this.drawIDOnCanvas();
+    this.drawOnCanvasFront();
+    this.drawOnCanvasBack();
   },
 };
 </script>
