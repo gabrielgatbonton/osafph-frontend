f18de1123ccbc8e0e7a13d853b65aa743bd872b6
gabrielgatbonton <115710319+gabrielgatbonton@users.noreply.github.com>
2023-10-30 21:53:43 +0800

    Improved Delete Dialog UI

--- src/components/ReusableDeleteDialog.vue
+++ src/components/ReusableDeleteDialog.vue
@@ -1,34 +1,56 @@
 <template>
   <div>
-    <v-dialog v-model="dialog" width="500">
+    <v-dialog v-model="dialog" max-width="450">
       <!-- <template v-slot:activator="{ on, attrs }">
         <v-icon v-bind="attrs" v-on="on" class="mx-1" color="error" dense
           >mdi-trash-can</v-icon
         >
       </template> -->
       <v-card>
-        <v-card-title class="text-h6 grey lighten-2 pb-5">
-          Delete
-        </v-card-title>
-
-        <v-card-text class="my-3 subtitle">
-          Are you sure you want to delete this item?
-        </v-card-text>
-
-        <v-divider></v-divider>
-
-        <v-card-actions>
-          <v-btn outlined color="primary" @click="dialog = false">Cancel</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn
-            dark
-            depressed
-            color="red"
-            :loading="loading"
-            @click="deleteItem"
-            >Delete</v-btn
-          >
-        </v-card-actions>
+        <v-container>
+          <v-row>
+            <v-col cols="12">
+              <v-img
+                class="mx-auto"
+                max-width="150"
+                src="@/assets/delete-1.svg"
+              ></v-img>
+            </v-col>
+            <v-col cols="12">
+              <div class="text-center text-h5 modal-title">Are you sure?</div>
+            </v-col>
+            <v-col cols="12">
+              <div class="text-center text-subtitle-1 modal-body">
+                Do you really want to delete this record? This process cannot be
+                undone.
+              </div>
+            </v-col>
+            <v-col cols="12">
+              <v-card-actions>
+                <div class="mx-auto">
+                  <v-btn
+                    large
+                    dark
+                    color="grey"
+                    class="px-10 mx-2"
+                    @click="dialog = false"
+                    >Cancel</v-btn
+                  >
+                  <v-btn
+                    dark
+                    large
+                    depressed
+                    class="px-10 mx-2"
+                    color="red"
+                    :loading="loading"
+                    @click="deleteItem"
+                    >Delete</v-btn
+                  >
+                </div>
+              </v-card-actions>
+            </v-col>
+          </v-row>
+        </v-container>
       </v-card>
     </v-dialog>
   </div>
@@ -49,7 +71,7 @@ export default {
   methods: {
     deleteItem() {
       this.$emit("deleteItem", true);
-    }
+    },
   },
   watch: {
     activator(newValue) {
@@ -64,4 +86,14 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.modal-title {
+  color: #424242;
+}
+.modal-body {
+  color: #999;
+  text-align: center;
+  margin: 0 auto;
+  width: 75%;
+}
+</style>
