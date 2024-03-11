d54a3d9dad2a8fd227471adaa6c807d276fc702e
gabrielgatbonton <115710319+gabrielgatbonton@users.noreply.github.com>
2024-03-11 22:31:34 +0800

    Dashboard Card

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,18 +1,21 @@
 <template>
   <v-container class="mx-auto grey">
-    <v-row>
-      <v-col>
-        <v-card>
-          <div class="d-flex justify-space-between px-4 py-2">
-            <div>Total Registrations</div>
-            <div>Icon</div>
+    <v-row class="mt-2">
+      <v-col cols="12" md="4">
+        <v-card class="pa-6">
+          <div class="d-flex justify-space-between align-center">
+            <div class="card-title">Total Registrations</div>
+            <div class="card-icon d-flex justify-center align-center">
+              <v-icon size="28" color="primary">mdi-account</v-icon>
+            </div>
           </div>
+          <div class="mt-5 card-number">18</div>
         </v-card>
       </v-col>
-      <v-col>
+      <v-col cols="12" md="4">
         <v-card> Test </v-card>
       </v-col>
-      <v-col>
+      <v-col cols="12" md="4">
         <v-card> Test </v-card>
       </v-col>
     </v-row>
@@ -23,3 +26,17 @@ export default {
   name: "DashboardView",
 };
 </script>
+<style scoped>
+.card-title {
+  font-size: 1.2rem;
+  font-weight: bold;
+}
+.card-number {
+  font-size: 2rem;
+  font-weight: bold;
+}
+.card-icon {
+  width: 50px;
+  height: 50px;
+}
+</style>
