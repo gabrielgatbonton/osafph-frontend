87710aa4e02091a345f7804fd332888483a03e40
Gabriel Gatbonton <115710319+gabrielgatbonton@users.noreply.github.com>
2024-03-25 15:56:35 +0800

    Updates to Login Page

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,79 +1,73 @@
 <template>
-  <div id="container">
-    <div id="gradient-section"></div>
-    <div></div>
-  </div>
-  <!-- <v-container fluid id="login-container">
-      <v-row justify="end">
-        <v-col cols="auto" align-self="center">
-          
-        </v-col>
-
-      </v-row>
-    </v-container> -->
-  <!-- <v-container fluid class="container-height v-scrollbar-hide">
-      <v-row>
-        <v-col cols="12" md="6" class="grey darken-4">
-          <div class="text-center text-container">
-            <div
-              class="headline white--text text-center mx-auto"
-              style="max-width: 300px"
-            >
-              OFFICE OF THE SPECIAL ADVISER FOR FLAGSHIP PROGRAM FOR HEALTH
-            </div>
-            <div>
-              <v-img
-                class="mx-auto d-block"
-                src="../assets/OSAFPH LOGO.png"
-                :max-width="$vuetify.breakpoint.xs ? 260 : 320"
-                contain
-              ></v-img>
-            </div>
+  <!-- <div id="container">
+    <div id="gradient-section">
+      <div id="image"></div>
+      <div id="circle"></div>
+    </div>
+  </div> -->
+  <!-- <v-container fluid id="container">
+    <v-row>
+      <v-col cols="12" md="6">
+        <div id="gradient-section"></div>
+      </v-col>
+      <v-col cols="12" md="6">
+      </v-col>
+    </v-row>
+  </v-container> -->
+  <v-container fluid id="container">
+    <v-row align="center" align-content="center" style="height: 100%">
+      <v-col cols="12" md="6">
+        <div class="text-container">
+          <v-img
+            class="mx-auto d-block"
+            src="../assets/lion.svg"
+            :max-width="$vuetify.breakpoint.xs ? 260 : 320"
+            contain
+          ></v-img>
+        </div>
+      </v-col>
+      <v-col cols="12" md="6">
+        <div class="card-container">
+          <div class="headline text-center mb-4">
+            DIALYSIS PATIENTS INFORMATION AND MANAGEMENT SYSTEM
           </div>
-        </v-col>
-        <v-col cols="12" md="6" :style="{ backgroundColor: '#d1d1d1' }">
-          <div class="card-container">
-            <v-card
-              rounded="xl"
-              class="mx-auto pb-7 slide-down"
-              max-width="350"
-              elevation="7"
-            >
-              <v-card-title class="justify-center blue--text headline"
-                >Login</v-card-title
-              >
-              <div class="mx-7">
-                <v-text-field
-                  v-model="username"
-                  :error-messages="usernameErrors"
-                  @blur="$v.username.$touch()"
-                  :success="!$v.username.$invalid && $v.username.$dirty"
-                  label="Username"
-                ></v-text-field>
-                <v-text-field
-                  v-model="password"
-                  :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-                  @blur="$v.password.$touch()"
-                  :error-messages="passwordErrors"
-                  :success="!$v.password.$invalid && $v.password.$dirty"
-                  :type="show ? 'text' : 'password'"
-                  @click:append="show = !show"
-                  label="Password"
-                ></v-text-field>
-                <v-btn
-                  class="mt-5 blue darken-2"
-                  block
-                  dark
-                  :loading="loading"
-                  @click="handleLogin"
-                  >Login</v-btn
-                >
-              </div>
-            </v-card>
-          </div>
-        </v-col>
-      </v-row>
-    </v-container> -->
+          <v-text-field
+            solo
+            rounded="lg"
+            height="50"
+            v-model="username"
+            :error-messages="usernameErrors"
+            @blur="$v.username.$touch()"
+            :success="!$v.username.$invalid && $v.username.$dirty"
+            label="Username"
+          ></v-text-field>
+          <v-text-field
+            solo
+            height="50"
+            rounded="lg"
+            v-model="password"
+            :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+            @blur="$v.password.$touch()"
+            :error-messages="passwordErrors"
+            :success="!$v.password.$invalid && $v.password.$dirty"
+            :type="show ? 'text' : 'password'"
+            @click:append="show = !show"
+            label="Password"
+          ></v-text-field>
+          <v-btn
+            x-large
+            rounded="lg"
+            class="blue darken-3"
+            block
+            dark
+            :loading="loading"
+            @click="handleLogin"
+            >Login</v-btn
+          >
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -174,16 +168,18 @@ export default {
   width: 100%;
   height: 100%;
   background: #fff;
-  #gradient-section {
-    background: linear-gradient(
-      to bottom,
-      rgba(251, 185, 185),
-      rgba(199, 123, 123),
-      rgba(157, 74, 74)
-    );
-    width: 50%;
-    height: 100%;
+  background: url("../assets/TestSVG.svg") no-repeat center center/cover;
+  transition: transform 0.3s ease; /* Add transition for smooth rotation */
 
+  .card-container {
+    max-width: 600px;
+    margin: 0 auto;
+  }
+}
+
+@media (max-width: 960px) {
+  #container {
+    background: none
   }
 }
 
