e6dabfdb5bf677d6d024349a9201a3fa37c8ce4e
gabrielgatbonton <115710319+gabrielgatbonton@users.noreply.github.com>
2024-04-14 16:00:28 +0800

    Scroll Behavior
    - Added Router Scroll Behavior
    - Saved Position
    - Reset to Top

--- src/App.vue
+++ src/App.vue
@@ -8,23 +8,27 @@
       "
     />
     <v-main>
+      <SubmissionAlert v-if="success.alert" :message="success.message" />
+      <ErrorAlert v-if="failed.alert" :message="failed.message" />
       <router-view />
     </v-main>
-    <FooterComponent /> 
+    <FooterComponent />
   </v-app>
 </template>
 
 <script>
 import NavComponents from "./layouts/NavComponents.vue";
 import FooterComponent from "./layouts/Footer/FooterComponent.vue";
+import ErrorAlertsLogic from "@/mixins/Alerts & Errors/ErrorAlertsLogic";
 export default {
   name: "App",
+  mixins: [ErrorAlertsLogic],
   data: () => ({
     //
   }),
   components: {
     NavComponents,
-    FooterComponent
+    FooterComponent,
   },
 };
 </script>
