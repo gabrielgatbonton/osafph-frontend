84258be71fb50bb7385c9024e9c90b6bea44c67d
gabrielgatbonton <115710319+gabrielgatbonton@users.noreply.github.com>
2024-08-01 01:38:30 +0800

    Added Header & Footer

--- src/components/Marketing/MarketingComponent.vue
+++ src/components/Marketing/MarketingComponent.vue
@@ -1,13 +1,26 @@
 <template>
-  <div>
-    
+  <div class="component-flex">
+    <!-- Header -->
+    <header-component />
+
+    <!-- Footer -->
+    <footer-component />
   </div>
 </template>
 
 <script>
+import FooterComponent from "./FooterComponent.vue";
+import HeaderComponent from "./HeaderComponent.vue";
 export default {
   name: "MarketingComponent",
+  components: { HeaderComponent, FooterComponent },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.component-flex {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+</style>
